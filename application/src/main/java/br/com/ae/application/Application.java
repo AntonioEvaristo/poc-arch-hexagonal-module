a5f02b4e416bf0694499c1009ad0f4364a00fb64
AntonioEvaristo <antonio.c.e.barbosa@outlook.com>
2022-09-28 20:06:18 -0300

    adicionando documentação com swagger

--- application/src/main/java/br/com/ae/application/Application.java
+++ application/src/main/java/br/com/ae/application/Application.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication(scanBasePackages = "br.com.ae")
 @EntityScan(basePackages = "br.com.ae.integration")
 @EnableJpaRepositories(basePackages = "br.com.ae.integration")
+@EnableSwagger2
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
